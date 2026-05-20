939dc1cd82a533e19f54b292d7ed0c2499238dc0
23081010076-oss <23081010076@student.upnjatim.ac.id>
2026-05-20 15:57:09 +0700

    feat: swap embed URLs and video IDs for 'Parikan Bahasa Jawa' entries in videoList

--- src/data/videos.js
+++ src/data/videos.js
@@ -2,8 +2,8 @@ export const videoList = [
   {
     title: 'Parikan Bahasa Jawa',
     description: 'Sinau parikan basa Jawa -- tegese, struktur, lan tuladha kanthi cara sing menarik lan gampang dingerteni.',
-    embedUrl: 'https://youtu.be/bgzSY9woZEE?si=P-5IAXFpPpHDMDzF',
-    videoId: 'bgzSY9woZEE',
+    embedUrl: 'https://youtu.be/X16jdQeqW6E?si=BiN7iZKdQQkDS1sx',
+    videoId: 'X16jdQeqW6E',
   },
 
   {
@@ -16,8 +16,8 @@ export const videoList = [
   {
     title: 'Parikan Bahasa Jawa',
     description: 'Sinau parikan basa Jawa -- tegese, struktur, lan tuladha kanthi cara sing menarik lan gampang dingerteni.',
-    embedUrl: 'https://youtu.be/X16jdQeqW6E?si=BiN7iZKdQQkDS1sx',
-    videoId: 'X16jdQeqW6E',
+    embedUrl: 'https://youtu.be/bgzSY9woZEE?si=P-5IAXFpPpHDMDzF',
+    videoId: 'bgzSY9woZEE',
   },
 
 ];
